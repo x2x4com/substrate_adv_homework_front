17ee9c08d41ff89d3f46f7aca6158765a617feef
Jacky <x2x4com@gmail.com>
2021-09-02 22:48:36 +0800

    2个问题，创建不刷新、转移不刷新

--- frontend/src/Kitties.js
+++ frontend/src/Kitties.js
@@ -13,30 +13,82 @@ export default function Kitties (props) {
   const [kitties, setKitties] = useState([])
   const [status, setStatus] = useState('')
 
+  // console.log(accountPair)
+
+  // let data = Array()
+  const [skittyIndex, setkittyIndex] = useState(0)
+  const [skittiesList, setkittiesList] = useState([])
+  const [sownersList, setownersList] = useState([])
+
   const fetchKitties = () => {
+    console.log(`fetchKitties start`)
+    let kittyIndex
+    let kittiesList = Array()
+    let ownersList = Array()
     // TODO: 在这里调用 `api.query.kittiesModule.*` 函数去取得猫咪的信息。
     // 你需要取得：
     //   - 共有多少只猫咪
     //   - 每只猫咪的主人是谁
     //   - 每只猫咪的 DNA 是什么，用来组合出它的形态
-    console.log(api)
+    // console.log(api)
+    // let kittyIndex
+    // let kittiesList = Array()
+    // let ownersList = Array()
     api.query.kittiesModule.kittiesCount(d => {
       // console.log(d)
-      let kittyIndex = d.unwrap().toNumber()
-      console.log(kittyIndex)
-      let kitties
-      api.query.kittiesModule.kitties(1).then(v => {
-        console.log(v.unwrap())
+      //let kittyIndex
+      if (d.isSome) {
+        kittyIndex = d.unwrap().toNumber()
+        // console.log(kittyIndex)
+      }
+      
+      //console.log('kittyIndex: ' + kittyIndex)
+      setkittyIndex(kittyIndex)
+      // let kittiesList = Array()
+      // let ownersList = Array()
+      // let ttt
+      // api.query.kittiesModule.kitties(kittyIndex - 1).then(v => {
+      //   if (v.isSome) {
+      //     ttt = v.unwrap().toU8a()
+      //   }
+      //   console.log(ttt[0])
+      // })
+      // let owner
+      // api.query.kittiesModule.owner(kittyIndex - 1).then(v => {
+      //   if (v.isSome) {
+      //     owner = v.unwrap().toString()
+      //   }
+      //   console.log(owner)
+      // })
+      const kittyIndexList = [...Array(kittyIndex).keys()]
+      api.query.kittiesModule.kitties.multi(kittyIndexList, (_kitties) => {
+        // console.log(_kitties)
+        for (let i in _kitties) {
+          if (_kitties[i].isSome) {
+            const _kitty = _kitties[i].unwrap().toU8a()
+            console.log(`${i} = ${_kitty}`)
+            kittiesList.push(_kitty)
+          }
+        }
+        //console.log('kittiesList: ' + kittiesList.join("|"))
+        setkittiesList(kittiesList)
       })
-      api.query.kittiesModule.owner(1).then(v => {
-        console.log(v.unwrap().toString())
-      })
-      api.query.kittiesModule.owner.multi([...Array(kittyIndex).keys()], (owners) => {
-        console.log(owners)
+      api.query.kittiesModule.owner.multi(kittyIndexList, (owners) => {
+        // console.log(owners)
+        // console.log(typeof(owners))
+        for (let i in owners) {
+          if (owners[i].isSome) {
+            const _owner = owners[i].unwrap().toString()
+            console.log(`${i} = ${_owner}`)
+            ownersList.push(_owner)
+          }
+        }
+        //console.log('ownersList: ' + ownersList.join("|"))
+        setownersList(ownersList)
       })
     })
-    // console.log(kittyIndex)
 
+    console.log(`fetchKitties end`)
   }
 
   const populateKitties = () => {
@@ -50,11 +102,30 @@ export default function Kitties (props) {
     //  ```
     // 这个 kitties 会传入 <KittyCards/> 然后对每只猫咪进行处理
     // const kitties = []
+    console.log(`populateKitties start`)
+    let kitties = Array()
+    // accountPair.address有地址信息
+
+    // 只显示自己的猫
+    for(let i = 0; i<skittyIndex; i++) { 
+      kitties.push({
+        id: i,
+        dna: skittiesList[i],
+        owner: sownersList[i]
+      })
+    }
+    // console.log('skittyIndex: ' + skittyIndex)
+    // console.log('skittiesList: ' + skittiesList.join("|"))
+    // console.log('sownersList: ' + sownersList.join("|"))
+    console.log(kitties)
     setKitties(kitties)
+    console.log(`populateKitties end`)
   }
 
-  useEffect(fetchKitties, [api, keyring])
-  useEffect(populateKitties, [])
+  useEffect(fetchKitties, [api, keyring, api.query.kittiesModule])
+  // useEffect(fetchKitties1, [api, keyring])
+  useEffect(populateKitties, [skittyIndex, skittiesList, sownersList])
+  //useEffect(populateKitties)
 
   return <Grid.Column width={16}>
     <h1>小毛孩</h1>
